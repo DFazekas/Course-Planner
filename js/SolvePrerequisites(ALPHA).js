738480bca0955e95ef8c9d5771c850d44b287b94
Devon Thomas <UmbraLegacy@users.noreply.github.com>
2017-01-13 23:13:34 -0500

    Segment CleanString() into 3 separate functions
    Broke up CleanString() into three separate functions; primaryStrCleaning(), secondaryStrCleaning() and formatString().

--- js/SolvePrerequisites(ALPHA).js
+++ js/SolvePrerequisites(ALPHA).js
@@ -1,18 +1,31 @@
 function main (string) {
-    return cleanString(string);
-}
+/* FUNCTION LEGEND
+ * 1. primaryStrCleaning():     cleans commonly occuring errors in string (returns string).
+ * 2. secondaryStrCleaning():   cleans unique errors in string (returns string).
+ * 3. formatString():           formats string to proper syntax (returns string).
+ */
 
-function cleanString (string){
+string = primaryStrCleaning (string);
+string = secondaryStrCleaning (string);
+string = formatString (string);
+
+return string;
+}                   // String -> String.
+
+
+function primaryStrCleaning (string) {
     /* FUNCTION LEGEND
-    * 1. trimOuterWhiteSpaces():    trims leading & trailing whitespaces from string (returns string).
-    * 2. trimTrailComma():          trims comma trailing string (returns string).
-    * 3. trimSpaceLeadComma():      trims whitespaces leading commas (returns string).
-    * 4. trimSpaceAdjParenth():     trims whitespaces adjacent parenthesis (returns string).
-    * 5. alphabetize():             converts @ symbol to alphabetical symbols (returns string).
-    * 6. n_of():                    converts "[1-3] of" patterns to "[1-3]of" patterns (returns string).
-    * 7. parenthToBrace():          converts parentheses to braces (returns string).
-    * 8. parenthEncaseStr():        encases string in parentheses (returns string).
-    */
+     * 1. trimOuterWhiteSpaces():    trims leading & trailing whitespaces from string (returns string).
+     * 2. trimTrailComma():          trims comma trailing string (returns string).
+     * 3. trimSpaceLeadComma():      trims whitespaces leading commas (returns string).
+     * 4. trimSpaceAdjParenth():     trims whitespaces adjacent parenthesis (returns string).
+
+     *
+     * Adds trailing whitespaces adjacent to commas.
+     * Replaces all instances of ',' with '+'. Problematic outside of ALPHA data.
+     * Replaces all instances of "and" with '+'. Problematic outside of ALPHA data.
+     *
+     */
     function trimOuterWhiteSpaces (string){
         return string.trim();
     }                            // String -> String.
@@ -25,6 +38,42 @@ function cleanString (string){
     function trimSpaceAdjParenth (string) {
         return (string.replace(/\s\)/g, ')')).replace(/\(\s/g, '(');
     }                            // String -> String.
+
+    string = trimOuterWhiteSpaces (string);
+    console.log("string = ", string, " {"+typeof string, " ", string.length+"} -- trimOuterWhiteSpaces()");
+
+    string = trimTrailComma (string);
+    console.log("string = ", string, " {"+typeof string, " ", string.length+"} -- trimTrailComma()");
+
+    string = trimSpaceLeadComma (string);
+    console.log("string = ", string, " {"+typeof string, " ", string.length+"} -- trimSpaceLeadComma()");
+
+    string = trimSpaceAdjParenth (string);
+    console.log("string = ", string, " {"+typeof string, " ", string.length+"} -- trimSpaceAdjParenth()");
+
+    return string;
+}     // String -> String.
+function secondaryStrCleaning (string) {
+    /* FUNCTION LEGEND
+     * 1. case01():      converts "1 or" patterns to "1 of" patterns.
+     */
+
+    function case01 (string) {
+        return string.replace(/(1)\sor/g, "$1 of");
+    }   // String -> String.
+
+    string = case01 (string);
+    console.log("string = ", string, " {"+typeof string, " ", string.length+"} -- case01()");
+    return string;
+}   // String -> String.
+function formatString (string) {
+    /* FUNCTION LEGEND
+     * 1. alphabetize():             converts @ symbol to alphabetical symbols (returns string).
+     * 2. n_of():                    converts "[1-3] of" patterns to "[1-3]of" patterns (returns string).
+     * 3. parenthToBrace():          converts parentheses to braces (returns string).
+     * 4. parenthEncaseStr():        encases string in parentheses (returns string).
+     */
+
     function alphabetize (string, arr = string.split(''), char = 65) {
         const result = arr => arr.reduce((a,b) => a.concat(b === '@' ? String.fromCharCode(char++) : b),[]);
         return result(arr).join('');
@@ -39,17 +88,17 @@ function cleanString (string){
         return '('+string+')';
     }                               // String -> String.
 
-    /* Cleaning string of errors. */
-    string = trimOuterWhiteSpaces (string);
-    string = trimTrailComma (string);
-    string = trimSpaceLeadComma (string);
-    string = trimSpaceAdjParenth (string);
-    
-    /* Converting string to proper form. */
     string = alphabetize (string);
+    console.log("string = ", string, " {"+typeof string, " ", string.length+"} -- alphabetize()");
+
     string = n_of (string);
+    console.log("string = ", string, " {"+typeof string, " ", string.length+"} -- n_of()");
+
     string = parenthToBrace (string);
+    console.log("string = ", string, " {"+typeof string, " ", string.length+"} -- parenthToBrace()");
+
     string = parenthEncaseStr (string);
-    
+    console.log("string = ", string, " {"+typeof string, " ", string.length+"} -- parenthEncaseStr()");
+
     return string;
-}
+}           // String -> String.
